1efd8eb457ead9cbb3a699ebb609ccc43573908c
Farid Khalikov <fkhalikov@gmail.com>
2019-02-26 21:39:38 +0000

    Added forward role and jumping

---
+++ rolling-cube/Assets/PlayerCollision.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public PlayerMovement playerMovement;
+
+    // Use this for initialization
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Obstacle")
+            playerMovement.enabled = false;
+    }
+}
---
+++ rolling-cube/Assets/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // Use this for initialization
+  
+	// Update is called once per frame
+	void FixedUpdate () {
+        
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime, ForceMode.Acceleration);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(0, 0, -forwardForce * Time.deltaTime, ForceMode.Acceleration);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(0, 100 * Time.deltaTime, 0, ForceMode.VelocityChange);
+        }
+    }
+}
